b7b88c374628e8c2dc82129602e0507e10f0ca9f
Kashev Jaswal <kjaswal@depaul.edu>
2023-09-26 22:01:18 -0500

    Adding folder for game to rep

---
+++ brickBreakerGame/Main.java
@@ -0,0 +1,17 @@
+package brickBreakerGame;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame obj = new JFrame();
+        Gameplay gameplay = new Gameplay();
+        obj.setBounds(10, 10, 700, 900);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(true);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+
+    }
+}
